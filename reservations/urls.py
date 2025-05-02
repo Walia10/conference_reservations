38b3cad07cd66112b99b55c7ea8fab8e89016324
Walia10 <130025360+Walia10@users.noreply.github.com>
2025-05-02 18:33:01 +1200

    basic interface

--- reservations/urls.py
+++ reservations/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('reserve/<int:room_id>/', views.make_reservation, name='make_reservation'),
     path('my-reservations/', views.my_reservations, name='my_reservations'),
+
 ]
